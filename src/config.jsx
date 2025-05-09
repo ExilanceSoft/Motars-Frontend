21b9ac7ca70eb30e69af13a4f75de2a4a2b89f7d
Devita Aher <exilancesoft@gmail.com>
2025-05-09 15:28:46 +0530

    Update config.jsx

--- src/config.jsx
+++ src/config.jsx
@@ -3,8 +3,8 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 const config = {
 //   baseURL: import.meta.env.REACT_APP_API_BASE_URL,
-    baseURL: 'http://192.168.1.11:5001/api'
-    // baseURL:"https://tvsapi.gandhitvs.in/api"
+    // baseURL: 'http://192.168.1.11:5001/api'
+     baseURL:"https://tvsapi.gandhitvs.in/api"
 };
 
 export default config;
